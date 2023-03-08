b9a9d307a8a907b23a5502c12553d4a91a9e4a2c
Pedro Silva <pedro.silva@cbyk.com.br>
2023-03-08 16:10:16 -0300

    feat: init board page

--- src/app/modules/to-do/pages/boards/boards.component.ts
+++ src/app/modules/to-do/pages/boards/boards.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'boards',
   templateUrl: 'boards.component.html',
+  styleUrls: ['./boards.component.scss'],
 })
 export class BoardsComponent implements OnInit {
   constructor() {}
